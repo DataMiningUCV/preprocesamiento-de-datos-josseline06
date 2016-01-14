7d56536f4940c16a001647bfc235ecb5481a2780
Josseline Perdomo <joss.desi@gmail.com>
2016-01-13 20:39:03 -0430

    Ordenado periodo académico

--- src/preprocessed.py
+++ src/preprocessed.py
@@ -7,10 +7,10 @@ import re
 input_data = pandas.read_csv('/dat/data.csv', header=None, skiprows=1)
 
 period_messy = input_data[1].str.replace('[ \\\\-]','')
-output_data = period_messy.str.extract('^(?P<period_year>(?:20)?[0-9]{2})(?P<period_nro>pri(?:mero)?|seg(?:undo)?|ii?|0?[12]s?)$', re.IGNORECASE)
-period_p2 = period_messy.str.extract('^(?P<period_nro>pri(?:mero)?|seg(?:undo)?|ii?|0?[12]s?)(?P<period_year>(?:20)?[0-9]{2})$', re.IGNORECASE)
+period = period_messy.str.extract('^(?P<year>(?:20)?[0-9]{2})(?P<nro>pri(?:mero)?|seg(?:undo)?|ii?|0?[12]s?)$', re.IGNORECASE)
+period_p2 = period_messy.str.extract('^(?P<nro>pri(?:mero)?|seg(?:undo)?|ii?|0?[12]s?)(?P<year>(?:20)?[0-9]{2})$', re.IGNORECASE)
 
-output_data.update(period_p2)
+period.update(period_p2)
 
-output_data.period_nro = output_data.period_nro.str.replace('pri(mero)?|i|0?1s?', '1', flags=re.IGNORECASE).str.replace('seg(undo)?|ii|0?2s?', '2', flags=re.IGNORECASE)
-output_data.period_year = output_data.period_year.str.replace('[0-9]{2}', '20[0-9]{2}', flags=re.IGNORECASE)
+period.nro = period.nro.str.replace('pri(mero)?|i|0?1s?', '1', flags=re.IGNORECASE).str.replace('seg(undo)?|ii|0?2s?', '2', flags=re.IGNORECASE)
+period.year = period.year.str.replace('^[0-9]{2}$', lambda str: '20'+str)
